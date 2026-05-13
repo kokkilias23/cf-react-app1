db7a4a6bc511bf1d57028ede8ad3577cf5bcb859
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-05-13 21:07:44 +0300

    fix: Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,12 @@
 
 const Footer = () => {
-  return (
+    new Date().getFullYear();
+    return (
     <>
-      <footer>
-        This is a Footer area!
+      <footer className="bg-cf-dark-gray text-white">
+          <div className="container mx-auto py-8 text-center">
+        &copy; 2026 Coding Factory 9, All Rights reserved.
+          </div>
       </footer>
     </>
   )
