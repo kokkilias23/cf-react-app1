c80b99d519c3888108fc95657753a0e2b48d105e
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-05-14 19:05:52 +0300

    fix: Footer and Layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
-
 const Footer = () => {
-  return (
-    <>
-      <footer>
-        This is a Footer area!
-      </footer>
-    </>
-  )
+    const currentYear: number = new Date().getFullYear()
+
+    return (
+        <>
+            <footer className="bg-cf-dark-gray text-white">
+                <div className="container mx-auto py-8 text-center">
+                    &copy; {currentYear} Coding Factory 9. All Rights reserved.
+                </div>
+            </footer>
+        </>
+    )
 }
 export default Footer;
+ 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,17 @@ import Header from './Header';
 import Footer from './Footer';
 
 interface LayoutProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const Layout = ({children}:LayoutProps) => {
-  return (
-    <>
-      <Header/>
-      <main>{children}</main>
-      <Footer/>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <main className="container mx-auto min-h-[calc(100vh-header-height)]">{children}</main>
+            <Footer/>
+        </>
+    )
 }
 
 export default Layout;
