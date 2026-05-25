4234385ab10a3ec8c1c85f402b17bfe3668d3396
dammarin <marinatosd@gmail.com>
2026-05-25 19:55:23 +0300

    feat: add MultifildForm component using html validation

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import CounterAdvanced from "./components/LessonFour/CounterAdvanced.tsx";
 import RouterLayout from "./components/RouterLayout.tsx";
 import UserPage from "./pages/UserPage.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
+import MultiFieldForm from "./components/LessonNine/MultiFieldForm.tsx";
 // import Counter from "./components/LessonFour/Counter.tsx";
 // import ClassCounter from "./components/LessonFour/ClassCounter.tsx";
 // import {useState} from "react";
@@ -165,6 +166,8 @@ function App() {
 
           <Route path="search" element={<SearchPage />} />
           {/*  /search?query=React&page=2  Query Params  */}
+
+          <Route path="multifield-form" element={<MultiFieldForm/>}/>
 
 
         </Route>
